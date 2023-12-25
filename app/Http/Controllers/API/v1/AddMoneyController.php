50c3655768d42b294c022c97696e2447094ff1d5
Tefoh <tefo.ha27@gmail.com>
2023-12-25 15:40:23 +0330

    feat(add-money): implement the validation to check amount cant be negative

--- app/Http/Controllers/API/v1/AddMoneyController.php
+++ app/Http/Controllers/API/v1/AddMoneyController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\API\v1\AddMoneyRequest;
 use App\Models\Wallet;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class AddMoneyController extends Controller
 {
@@ -14,6 +15,20 @@ class AddMoneyController extends Controller
         $data = $request->toArray();
 
         $data['reference_id'] = rand(10000, 999999999);
+
+        $sum = Wallet::query()
+            ->where('user_id', $data['user_id'])
+            ->sum('amount');
+
+        $sum += $data['amount'];
+
+        if ($sum < 0) {
+            throw ValidationException::withMessages([
+                'amount' => [
+                    'Entered amount for this user is more than current amount of wallet'
+                ]
+            ]);
+        }
 
         Wallet::query()->create($data);
 
